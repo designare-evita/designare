a26469e88d5f73d6247be57808ef883af8f27b06
Michael <designare.design@gmail.com>
2025-07-22 20:03:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // --- H1-TYPEWRITER ---
     const typewriterElement = document.getElementById('typewriter-h1');
     if (typewriterElement) {
-        const textsToType = [ "Michael Kanda", "Web-Flüsterer", "KI-Versteher (meistens)" ];
+        const textsToType = [ "Michael Kanda", "Web-Flüsterer", "KI-Therapeut" ];
         let textIndex = 0; let charIndex = 0; let isDeleting = false;
         const typingSpeed = 110, deletingSpeed = 55, delayBetweenTexts = 2000;
 
