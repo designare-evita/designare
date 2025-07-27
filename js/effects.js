a7ef9fa5e4cae656d6d4304a5eb341276b4e7e6f
Michael <designare.design@gmail.com>
2025-07-27 07:09:32 +0200

    Update effects.js

--- js/effects.js
+++ js/effects.js
@@ -3,12 +3,82 @@
 // Initialisiert den Partikel-Hintergrund
 function setupParticles() {
     if (document.getElementById('particles-js')) {
-        particlesJS("particles-js", {
-            // ... (Ihre komplette Partikel-Konfiguration hier einfügen)
-            "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#888888" }, "shape": { "type": "circle" }, "opacity": { "value": 0.5, "random": true }, "size": { "value": 3, "random": true }, "line_linked": { "enable": true, "distance": 150, "color": "#cccccc", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 2, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false } },
-            "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "repulse": { "distance": 100, "duration": 0.4 }, "push": { "particles_nb": 4 } } },
-            "retina_detect": true
-        });
+        particlesJS("particles-js", 
+  {
+    // Dies ist Ihre Konfiguration für die Partikel
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#888888"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#cccccc",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "repulse": {
+          "distance": 100,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        }
+      }
+    },
+    "retina_detect": true
+  }, 
+  // Dies ist die wichtige Callback-Funktion
+  function() {
+    console.log('Partikel-Animation ist jetzt bereit.');
+    
+    // Rufen Sie HIER die Funktion auf, die das Theme und die Farben anwendet.
+    // Dadurch wird der Fehler vermieden.
+    initTheme(); 
+  }
+);
     }
 }
 
